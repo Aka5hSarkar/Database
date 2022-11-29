6a9c99fb553fb4e2ad9162570f7989fb87fb05a1
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-11-29 18:31:51 +0530

    Add files via upload

--- Joining.sql
+++ Joining.sql
@@ -35,3 +35,25 @@ select fname,lname,amount,course from Student_join inner join fees on Student_jo
 select * from student_join cross join fees;
 #self join
 select s1.fname,s1.age from student_join s1 inner join student_join s2 on s1.age=s2.age and s1.id<>s2.id order by s1.id;
+#sub query
+select * from student_join where id in(select id from student_join where age>23);
+select * from fees where amount=(select max(amount) from fees );
+select * from student_join where city not in(select city from student_join where city ='Kolkata');
+#any clause
+#comparison between a single column value and a range of other values...(ANY and ALL)
+#any returns the boolean value if any of the subquery value meet the condition
+select fname,form_no from student_join where id>any(select Form_no from fees);
+select amount,course from fees where amount>any(select amount from fees);
+#all 
+select form_no,fname from student_join where id>all(select Form_no from fees);
+#not_exists
+select fname,age from student_join where not exists(select * from fees where student_join.form_no=fees.form_no);
+#rollup
+select form_no,count(course),sum(amount) as 'new_column' from fees group by form_no with rollup;
+#outer join--left
+select student_join.form_no,fname,course from student_join left join fees on student_join.form_no=fees.form_no;
+#right outer join
+select student_join.form_no,fname,course from student_join right join fees on student_join.form_no=fees.form_no;
+#full outer join
+select student_join.form_no,student_join.fname,fees.course,fees.amount from student_join full join fees on student_join.Form_no=fees.form_no;
+
